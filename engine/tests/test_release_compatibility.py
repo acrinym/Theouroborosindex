a34ece8d0b2aa7492e72054e0bfa7bd16bb41de0
acrinym <acrinym@gmail.com>
2026-08-11 01:02:35 -0400

    Avoid stacked release compatibility test conflicts

--- engine/tests/test_release_compatibility.py
+++
@@ -1,7 +0,0 @@
-from ouroboros.neighbors import MEASUREMENT_MODEL, _semantic_model_for_version
-
-
-def test_0_11_keeps_the_existing_semantic_measurement_generation_closed_ended():
-    assert _semantic_model_for_version("0.11.0") == MEASUREMENT_MODEL
-    assert _semantic_model_for_version("0.11.0.dev0") == MEASUREMENT_MODEL
-    assert _semantic_model_for_version("0.12.0") is None
